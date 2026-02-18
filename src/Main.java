dbb0221ba663dc6347b251dad2e3050a3ebfd121
User <grinevich697@gmail.com>
2026-02-18 17:59:42 +0300

    home

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Привет мир!");
-    System.out.println("Hello");
+        System.out.println("Привет мир!");
+        int number = 2323232;
+        System.out.println(number);
     }
 }
