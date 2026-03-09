577a02ba240902f9524afb6df953d03030b3d3a5
User <grinevich697@gmail.com>
2026-03-09 22:22:49 +0300

    cycle while and do-while

--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,91 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        // Задача 1
+        int firstFriday = 3;
+        for (int i = 1; i <= 31; i++) {
+            if ((i - firstFriday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+            }
         }
-
         //Задача 2
-        for (int num = 10; num > 0; num--) {
-            System.out.println(num);
+        int distanceTraveled = 0;
+        do {
+            System.out.println("Держитесь! Осталось " + distanceTraveled + " метров ");
+            distanceTraveled += 500;
         }
-        //Задача 3
-        for (int num1 = 0; num1 <= 17; num1 += 2) {
-            System.out.println(num1);
+        while (distanceTraveled <= 42195);
+        //Задача 2 вариан с циклом for
+        for (int i = 0; i <= 42195; i += 500) {
+            System.out.println("Держитесь! Осталось " + i + " метров ");
+        }
+        //Задача 3.1
+        int budget = 1000;
+        int day = 0;
+        while (budget > 0) {
+            day++;
+            if (day % 5 == 0) {
+                continue;
+            }
+            budget -= 100;
+        }
+        System.out.println("Количество дней равно - " + day);
+
+        // Задача 3.2
+        int balance = 1100;
+        int parkingDay = 0;
+        int i = 0;
+        for (; i < balance; i++) {
+            parkingDay++;
+            balance -= 100;
+
+            if (parkingDay % 5 == 0) {
+                continue;
+
+            }
+            System.out.println("Количество дней равно - " + parkingDay);
         }
         //Задача 4
-        for (int num2 = 10; num2 >= -10; num2--) {
-            System.out.println(num2);
+        int month = 0;
+        double total = 0;
+        while (total < 12_000_000) {
+            month++;
+            total = total + 15_000;
+            if (month % 6 == 0) {
+                total = total * 1.07;
+                {
+                    System.out.printf("Месяц %d Итого %.2f \n", month, total);
+                }
+                if (total >= 12_000_000) {
+                    break;
+                }
+
+            }
         }
         //Задача 5
-        for (int leapYear = 1904; leapYear < 2096; leapYear += 4) {
-            System.out.println(leapYear + " год является високосным");
-        }
-        //Задача 6
-        for (int num3 = 7; num3 <= 98; num3 += 7) {
-            System.out.println(num3);
-        }
-        //Задача 7
-        for (int num4 = 1; num4 <= 512; num4 *= 2) {
-            System.out.println(num4);
-        }
-        //Задача 8
-        int salary = 29000;
-        int total = 0;
-        for (int i  = 0; i < 12; i++) {
-            total = total + salary;
-            System.out.println("Месяц " + i +","+ "сумма накоплений равна " + total + " рублей");
-        }
-       // Задача 9
-        int salary1 = 29000;
-        int total1 = 0;
-        for (int i = 0; i < 12; i++) {
-            total1 = total1 + total1/100;
-            total1 = total1 + salary1;
-            System.out.println("Месяц " + i + "," + "сумма накоплений равна " + total1 + " рублей");
-        }
-        //Задача 10
-        int num = 2;
-        int result;
-        for (int i = 1; i <= 10; i++) {
-            result = num * i;
-            System.out.println(num + "*" + i + "=" + result);
-        }
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
+        while (charge < 100 && overheats <=3) {
+            minute++;
+            charge += 2;
+            if (minute % 10 == 0) {
+                overheats++;
+                minute++;
+                continue;
+            }
+            System.out.println("Зарядка прекращена. Текущий заряд: " + charge + " %.");
+            if (overheats == 3) {
+                break;
+            }
+            System.out.println("Время зарядки составило " + minute + " минут.");
+
         }
     }
+}
+
+
+
 
 
 
