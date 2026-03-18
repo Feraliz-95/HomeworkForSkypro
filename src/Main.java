7521ef6ec9e393aeae05402da6c36f0654869ed0
User <grinevich697@gmail.com>
2026-03-18 23:28:12 +0300

    Arrays. Array Comparison

--- src/Main.java
+++ src/Main.java
@@ -4,60 +4,91 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        int[] arr = new int[] {1, 2, 3};
-        //Задача 1.1
-        double [] array = {1.57,7.654, 9.986};
-        //Задача 1.2
-        int[] arrays = new int[] {15, 26, 44, 92, 100};
+        int[] inputArray1 = {91, 1115, 765, 350, 400};
+        int outputArray1 = 0;
+        int sum = 0;
+        int maxSum = 0;
+        int min = inputArray1[0];
+        float middleSum = 0;
+
+        for (int element : inputArray1) {
+            sum = sum + element;
+        }
+        System.out.println("Сумма выплат: " + sum);
+        for (int max : inputArray1) {
+            if (max > maxSum) {
+                maxSum = max;
+            }
+        }
+        System.out.println("Максимальная выплата: " + maxSum);
+
+        for (int minNums : inputArray1) {
+            if (minNums < min) {
+                min = minNums;
+            }
+        }
+        System.out.println("Минимальная выплата: " + min);
+        for (float mid : inputArray1) {
+            middleSum += mid;
+        }
+        middleSum = middleSum / inputArray1.length;
+        System.out.println("Среднее значение: " + middleSum);
+
         //Задача 2
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(arrays));
-
+        int[] inputArray2 = {800, 2500, 4300, 200, 3250};
+        float[] outputArray2 = new float[5];
+        int index = 0;
+        for (float salary : inputArray2) {
+            float tax = salary * 0.13f;
+            outputArray2[index] = tax;
+            index++;
+            System.out.println(tax);
+        }
         //Задача 3
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arr[i]);
-            } else {
-                System.out.print(arr[i] + ", ");
+        int[] inputArray3 = {3000, 4500, 6000, 5300, 7000};
+        boolean[] outputArray3 = new boolean[inputArray3.length];
+        int index1 = 0;
+        for (int bonus : inputArray3) {
+            if (bonus > 5000) {
+                outputArray3[index1] = true;
+            }else{
+                outputArray3[index1] = false;
             }
+            index1++;
         }
-        System.out.println();
-        //Задача 3.1
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(array[i]);
-            } else {
-                System.out.print(array[i] + ", ");
-            }
+        for (boolean flag : outputArray3) {
+            System.out.println(flag);
         }
-        System.out.println();
-
-        //Задача 3.2
-        for (int i = arrays.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arrays[i]);
-            } else {
-                System.out.print(arrays[i] + ", ");
-            }
-        }
-        System.out.println();
 
         //Задача 4
-        int [] num = {1, 2, 3, 5};
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] % 2 !=0) {
-                num[i]++;
-                System.out.print(num[i] + " ");
-
+        int[] inputArray4 = {100, 50, 25, -10, 30};
+        boolean outputArray4 = true;
+        for (int balance : inputArray4) {
+            if (balance < 0) {
+                outputArray4 = false;
+                break;
             }
         }
+        System.out.println(outputArray4);
 
+        //Задача 5
+        int[] inputArray5 = {100, -50, 200, 0, 300};
 
+        // Переменная для подсчета прибыльных месяцев
+        int outputArray5 = 0;
+
+        // Цикл for-each
+        for (int profit : inputArray5) {
+            if (profit > 0) {
+                outputArray5++;
+            }
         }
+        System.out.println("Количество прибыльных месяцев: " + outputArray5);
 
+
+
+    }
 }
-
 
 
 
