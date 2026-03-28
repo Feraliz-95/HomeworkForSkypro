ddabce4b3c8d4bd08f21c1b92209a70743772802
User <grinevich697@gmail.com>
2026-03-28 15:52:33 +0300

    Method HW

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,52 @@
-
-import java.util.Arrays;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Задача 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф.И.О. сотрудника — " + fullName);
-
-        System.out.println("Задача 2");
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
-
-        System.out.println("Задача 3");
-        String fullName1 = "Иванов Семён Семёнович";
-        String correctedFullName = fullName1.replace("ё", "e");
-        System.out.println("Данные Ф. И. О. сотрудника — " + correctedFullName);
-
+    //Задача 1
+    public static void leapYears(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
-}
+
+    //Задача 2
+    public static void softwareDefinition(int clientDeviceYear, int clientOs1) {
+        if (clientOs1 == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOs1 == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+        if (clientOs1 == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOs1 == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+    }
+
+    //Задача 3
+    public static void distanceOfDays(int deliveryDistance) {
+        int days = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + days);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            days += 1;
+            System.out.println("Потребуется дней: " + days);
+        } else if  (deliveryDistance >= 60 && deliveryDistance < 100) {
+            days += 2;
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Свыше 100 км доставки нет.");
+        }
+    }
+    public static void main(String[] args) {
+    leapYears(2024);
+    softwareDefinition(2017, 1);
+    distanceOfDays(102);
+        }
+    }
+
+
+
+
 
 
 
