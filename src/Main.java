a38706ca4930b2ae7bc269a119c8d8ab465316ae
User <grinevich697@gmail.com>
2026-04-07 19:43:03 +0300

    Methods of the class object

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,35 @@
+
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Лев", "Толстой");
-        Book book = new Book("Война и мир", author, 1869);
+        System.out.println("Задание 1");
+        Product product1 = new Product(1, "Монитор", 25000, "Мониторы");
+        System.out.println(product1);
+        Product product2 = new Product(2, "Умная колонка", 15000, "Электроника");
+        System.out.println(product2);
+        Product product3 = new Product(2, "Smart Часы", 23000, "Гаджеты");
+        System.out.println(product3);
+        Product product4 = new Product(1, "Монитор", 25000, "Мониторы");
+        System.out.println(product4);
+        Product product5 = new Product(1, "Смартфон", 47000, "Гаджеты");
+        System.out.println(product5);
 
-        System.out.println("Название: " + book.getTitle());
-        System.out.println("Автор: " + book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName());
-        System.out.println("Год публикации: " + book.getPublicationYear());
-
-        Author author1 = new Author("Федор", "Достаевский");
-        Author author2 = new Author("Оскар", "Уайльд");
-
-
-        Book book1 = new Book("Белые ночи", author1, 1848);
-        Book book2 = new Book("Идиот", author1, 1869);
-        Book book3 = new Book("Портрет Дориана Грея", author2, 1890);
-        Book book4 = new Book("Счастливый принц", author2, 1888);
-        book1.setPublicationYear(1850);
-        System.out.println("book1 = " + book1);
-        System.out.println("book2 = " + book2);
-        System.out.println("book3 = " + book3);
-        System.out.println("book4 = " + book4);
+        System.out.println("Задание 2");
+        System.out.println(product1.equals(product4));
 
 
+        System.out.println("Задание 3");
+        Order o1 = new Order("Иван", new Product[]{product1, product2, product3});
+        Order o2 = new Order("Петр", new Product[]{product2, product5, product4});
+        Order o3 = new Order("Иван", new Product[]{product4, product3, product1});
+        System.out.println("Order 1: " + o1);
+        System.out.println("Order 2: " + o2);
+        System.out.println("Order 3: " + o3);
+        System.out.println(product1.equals(product3));
 
     }
 
 
-    }
+}
 
 
 
