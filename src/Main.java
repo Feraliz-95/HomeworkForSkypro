7f0c4b43477ffd8c88f7b9967d24f8758cc727ce
User <grinevich697@gmail.com>
2026-06-26 01:18:17 +0300

    HW-22 Java Collections Framework: Set

--- src/Main.java
+++ src/Main.java
@@ -15,30 +15,23 @@ public class Main {
 
         // Создаём поисковый движок
 
-        List<Searchable> products = new ArrayList<>();
-        products.add(new SimpleProduct("Banana", 28));
-        products.add(new DiscountedProduct("Apple", 100, 25));
-        products.add(new FixPriceProduct("Orange"));
-        products.add(new SimpleProduct("Apple", 100)); // Можно добавить несколько одинаковых продуктов
-
-        SearchEngine searchEngine = new SearchEngine();
-        Map<String, Searchable> searchResults = searchEngine.search(products);
-
+        Set<Searchable> items = new HashSet<>();
+        items.add(new Article("Java", "Basics"));
+        items.add(new Article("JavaScript", "JavaScript for beginners"));
+        items.add(new Article("Java Advanced", "Programming"));
+        items.add(new SimpleProduct("Apple", 28));// будет добавлен
+        items.add(new SimpleProduct("Apple", 28));// не будет добавлен, дубликат
+        items.add(new SimpleProduct("Banana", 55));
+        items.add(new DiscountedProduct("kiwi", 100, 25));
+        items.add(new FixPriceProduct("egg"));
         // Вывод результатов поиска
-        System.out.println("Search Results (sorted by name):");
-        for (Map.Entry<String, Searchable> entry : searchResults.entrySet()) {
-            String productName = entry.getKey();
-            Product product = (Product) entry.getValue(); // Приведение типа обратно к Product
-            System.out.println("Product Name: " + productName + ", Price: " + product.getPrice());
+        System.out.println("Список статей и продуктов:");
+        for (Object item : items) {
+            System.out.printf("- %s%n", item);
+        }
+
         }
     }
-}
-
-
-
-
-
-
 
 
 
