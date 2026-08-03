7de2cd9afca6a927e16da5ab9496e3ebf7e91153
User <grinevich697@gmail.com>
2026-08-03 21:07:50 +0300

    Initial commit

---
+++ src/test/java/org/example/exam_questions/ExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exam_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
